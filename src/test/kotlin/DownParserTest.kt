32d156097ca7b52cc39d4330c61bb07a46a07619
Alex Plate <AlexPl292@gmail.com>
2020-10-21 16:49:15 +0300

    Add more tests

--- src/test/kotlin/DownParserTest.kt
+++ src/test/kotlin/DownParserTest.kt
@@ -9,6 +9,18 @@ class DownParserTest {
     @Test
     fun `test header 2`() = assertParsing("## Header", "*Header*\n")
 
+    @Test
+    fun `test strong`() = assertParsing("**strong**", "*strong*")
+
+    @Test
+    fun `test emph`() = assertParsing("*strong*", "_strong_")
+
+    @Test
+    fun `test strong 2`() = assertParsing("__strong__", "*strong*")
+
+    @Test
+    fun `test emph 2`() = assertParsing("_strong_", "_strong_")
+
     private fun assertParsing(md: String, slack: String) {
         val result = DownParser(md).toSlack()
         assertEquals(slack, result)
